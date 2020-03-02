df9a6b70a6f91e5a62ebd2a683af7a5df6c2e128
ndrekbrg <ndrzedq@gmail.com>
2020-03-02 08:42:15 +0400

    docker fix

--- src/api.py
+++ src/api.py
@@ -72,4 +72,4 @@ api.add_resource(Story, '/stories/<story_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 LIMIT = 30
 API_KEY = '15445271-4ebc3d653469c2411b4fe6211'
-FILE = 'sample.json'
+FILE = '/code/src/sample.json'
